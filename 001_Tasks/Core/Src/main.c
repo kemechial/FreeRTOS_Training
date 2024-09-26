085e3b5de7a0c39322d8c7a21cf761dbe2c0316f
kemechial <kaanevran@gmail.com>
2024-09-26 21:48:01 +0300

    ITM console error fixed...

--- 001_Tasks/Core/Src/main.c
+++ 001_Tasks/Core/Src/main.c
@@ -21,6 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -98,6 +99,8 @@ configASSERT(status == pdPASS);
 
 status = xTaskCreate(Task2_Handler, "Task_2", 200, "Message from task 2", 2, &task2_handle);
 configASSERT(status == pdPASS);
+
+vTaskStartScheduler(); //if the scheduler fails, this function will return.
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -135,7 +138,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -147,8 +150,8 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
@@ -302,6 +305,10 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 static void Task1_Handler(void* parameters){
 
+	while(1){
+		printf("%s\n", (char *) parameters);
+		taskYIELD();
+	}
 
 
 
@@ -309,7 +316,36 @@ static void Task1_Handler(void* parameters){
 
 static void Task2_Handler(void* parameters)
 {
+	while(1){
+		printf("%s\n", (char *) parameters);
+		taskYIELD();
+	}
 
+}
+
+
+int _write(int le, char *ptr, int len)
+
+
+{
+
+
+int DataIdx;
+
+
+for(DataIdx = 0; DataIdx < len; DataIdx++)
+
+
+{
+
+
+ITM_SendChar(*ptr++);
+
+
+}
+
+
+return len;
 
 
 }
